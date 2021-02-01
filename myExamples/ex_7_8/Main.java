e74b8ac9b9ddef8e87eb177f2f9611c1c2502265
Savvas Filippidis <filippides.savvas@gmail.com>
2021-02-01 21:32:44 +0200

    ex_7_8_a

--- myExamples/ex_7_8/Main.java
+++ myExamples/ex_7_8/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main{
 
 	public static void main(String[] args)
@@ -5,39 +7,60 @@ public class Main{
 
 		int[] f = new int[10];
 
-		System.out.println(f[6]);
+		System.out.println(f[5]);
 
-		int[] g = {8, 8, 8, 8, 8};
+		int[] g = new int[5];
+		printArray(g);
+		Arrays.fill(g, 8);
+		printArray(g);
+
+
 
 		float[] c = new float[100];
+		Arrays.fill(c, 1.0f);
 		float sum = 0.0f;
 
-		for (int counter = 0; counter < c.length; counter++)
-			sum += c[counter];
+		for (float value : c)
+			sum += value;
 
 		System.out.println(sum);
 		
 		int[] a = new int[11];
+		Arrays.fill(a, 7);
 		int[] b = new int[34];
-
-		for (int counter = 0; counter < a.length; counter++)
-			b[counter] = a[counter];
+		
+		System.arraycopy(a, 0, b, 0, 11);
+		System.out.println();
+		printArray(a);
+		System.out.println();
+		printArray(b);
+		System.out.println();
 
 		float[] w = new float[99];
 		float min = w[0];
 		float max = w[0];
 
-		for (int counter = 1; counter < w.length; counter++)
+		for (float value : w)
 		{
 
-			if (w[counter] < min)
-				min = w[counter];
+			if (value < min)
+				min = value;
 
-			if (w[counter] > max)
-				max = w[counter];
+			if (value > max)
+				max = value;
 		}
+		System.out.println(min);
+		System.out.println(max);
+
+	}
 
 
+	public static void printArray(int[] array)
+	{
+		for (int e : array)
+		{
+			System.out.println(e);
+		}
 	}
 
 }
