c096ba5262823a7e20b41a683214a27b2b41974e
Savvas Filippidis <filippides.savvas@gmail.com>
2021-02-08 22:55:11 +0200

    ex_7_22

---
+++ myExamples/ex_7_22/Main.java
@@ -0,0 +1,23 @@
+public class Main{
+
+	private static final int ROWS = 8;
+	private static final int COLUMNS = 8;
+	private static final int SQUARES = 64;
+	
+
+	public static void main(String[] args)
+	{
+		int[][] board = new int[ROWS][COLUMNS];
+		int[] horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
+		int[]   vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
+		
+		int currentRow = 0;
+		int currentColumn = 0;
+
+		for (int counter = 1; counter <= SQUARES; counter++)
+		{
+			
+		}
+
+	}
+}
